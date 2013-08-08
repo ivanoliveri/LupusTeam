e181bd5c73525d3429090b831dbd4fdbcd4f060f
Ivan P. Oliveri <ipoliveri@gmail.com>
2013-08-08 17:53:56 -0300

    Agrego mas secciones.

---
+++ Web/Web/Controllers/ContactController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class ContactController : Controller
+    {
+        //
+        // GET: /Contact/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Contact(string clientMessage, string mail, string fullName,string phone,string enterprise)
+        {
+            var fromAddress = new MailAddress("lupusteam.projects@gmail.com@gmail.com", fullName);
+            var toAddress = new MailAddress("lupusteam.projects@gmail.com@example.com");
+            const string fromPassword = "skullsett";
+            const string subject = "Subject";
+            const string body = "Body";
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+            };
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject,
+                Body = body
+            })
+            {
+                smtp.Send(message);
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ Web/Web/Controllers/TecnologyController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class TecnologyController : Controller
+    {
+        //
+        // GET: /Tecnology/
+
+        public ActionResult IndexFrontend()
+        {
+            return View("Frontend");
+        }
+
+        public ActionResult IndexBackend()
+        {
+            return View("Backend");
+        }
+
+        public ActionResult IndexDatabase()
+        {
+            return View("Database");
+        }
+
+    }
+}
